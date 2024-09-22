c580f61ae8159a5111d17e4259a2d29001d89454
Karume-lab <danielkarume@outlook.com>
2024-09-22 04:41:39 +0300

    feat: :lipstick: add skeletons for posts

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -1,6 +1,7 @@
 "use client";
 import InfiniteScrollContainer from '@/components/InfiniteScrollContainer';
 import Post from '@/components/posts/Post';
+import PostsLoadingSkeleton from '@/components/posts/PostsLoadingSkeleton';
 import { Button } from '@/components/ui/button';
 import kyInstance from '@/lib/ky';
 import { PostPage } from '@/lib/types';
@@ -31,9 +32,11 @@ const ForYouFeed = () => {
 
 
     if (status === "pending") {
-        return <Loader2 className='mx-auto animate-spin' />
+        return <PostsLoadingSkeleton />
     } else if (status === "error") {
         return <p className='text-center text-destructive'>An error occurred while loading posts</p>
+    } else if (status === "success" && !posts.length && !hasNextPage) {
+        return <p className='text-center text-muted-foreground'>No one has posted anything yet.</p>
     }
 
     return (
